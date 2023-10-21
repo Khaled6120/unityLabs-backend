0cd45955d26d654ccd2dbdcd188d344b4c57c56e
Khaled6120 <khalednaes3@gmail.com>
2023-10-21 23:53:36 +0300

    add error handlers middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 import express from 'express';
 import bodyParser from 'body-parser';
 import connectToDatabase from './db/connection.js';
+import { StatusCodes } from 'http-status-codes';
+
+import { AppError } from './utils/apiError.js';
+import { errorHandlerMiddleware } from './utils/globalErrorHandler.js';
 
 dotenv.config();
 
@@ -15,9 +19,27 @@ connectToDatabase();
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World with Express'));
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the Distribution API' }));
+
+// generating error for undefined routes
+app.all('*', (req, res, next) => {
+    next(
+        new AppError(
+            StatusCodes.BAD_REQUEST,
+            `This route: ${req.originalUrl} is not exist!`
+        )
+    );
+});
+
+// a global error middleware that catch errors and present them in a structured way.
+app.use(errorHandlerMiddleware);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
+process.on('unhandledRejection', (reason) => {
+    console.error(`Unhandled Rejection: ${reason.message || reason}`);
+});
+
+export default app;
