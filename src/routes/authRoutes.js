194537f3744563608f3e795d610f89b12b19a6ac
Khaled6120 <khalednaes3@gmail.com>
2023-10-22 12:58:53 +0300

    Add validation layer to auth routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { signinUser, registerUser } from '../controllers/auth.js';
+import { signupValidator, signinValidator } from '../utils/validators/authValidtor.js';
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/register', signupValidator, registerUser);
 
-router.post('/login', signinUser);
+router.post('/login', signinValidator, signinUser);
 
 
 export default router
