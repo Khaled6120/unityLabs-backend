f82fb0f4e27bdb2bff48f16ec86a3a257ad60a1e
Khaled6120 <khalednaes3@gmail.com>
2023-10-22 04:55:28 +0300

    Add create order functionality

--- src/controllers/buyer.js
+++ src/controllers/buyer.js
@@ -4,11 +4,12 @@ import { StatusCodes } from 'http-status-codes';
 import { AppError } from '../utils/apiError.js';
 import User from '../models/user.js';
 import Catalog from '../models/catalog.js';
+import Order from '../models/order.js';
 
 
 // @desc    Get a list of all sellers
 // @route   Get /api/buyer/list-of-sellers
-// @access  Public
+// @access  buyers
 const getListOfSellers = asyncHandler(async (req, res) => {
     const sellers = await User.find({ role: 'seller' }, { name: 1, email: 1, _id: 1 });
     if (!sellers) {
@@ -19,7 +20,7 @@ const getListOfSellers = asyncHandler(async (req, res) => {
 
 // @desc    Get the catalog of a seller by seller_id
 // @route   Get /api/buyer/seller-catalog/:seller_id
-// @access  Public
+// @access  buyer
 const getSellerCatalog = asyncHandler(async (req, res) => {
     console.log(req.params.seller_id)
     const seller = await User.findById(req.params.seller_id);
@@ -30,4 +31,54 @@ const getSellerCatalog = asyncHandler(async (req, res) => {
     res.status(StatusCodes.OK).json(catalog);
 });
 
-export { getListOfSellers, getSellerCatalog };
+// @desc    Create order
+// @route   Post /api/buyer/create-order/:seller_id
+// @access  buyer
+const createOrder = asyncHandler(async (req, res, next) => {
+
+    const { seller_id } = req.params;
+    const { items } = req.body;
+
+    // Check if seller exists
+    const seller = await User.findById(seller_id);
+    if (!seller || seller.role !== 'seller') {
+        return next(new AppError(StatusCodes.NOT_FOUND, 'This is not a valid seller ID'));
+    }
+    // find seller's catalog
+    const sellerCatalog = await Catalog.findOne({ seller: seller_id });
+
+    // Check if seller has any products in their catalog
+    if (!sellerCatalog.products.length || !items.length) {
+        return next(new AppError(StatusCodes.BAD_REQUEST, 'No products in the catalog or no items in the order'));
+    }
+
+    // Check if all items belong to the same seller
+    const itemSellers = new Set(items.map(item => sellerCatalog.products.find(product => product.name.toUpperCase() === item.name.toUpperCase())));
+    itemSellers.delete(undefined);
+    if (items.length !== itemSellers.size) {
+        console.log(items.length, itemSellers.size)
+        return next(new AppError(StatusCodes.BAD_REQUEST, 'All items should belong to the same sellerr'));
+    }
+
+    // Calculate total price
+    const totalPrice = items.reduce((total, item) => {
+        const product = sellerCatalog.products.find(product => product.name.toUpperCase() === item.name.toUpperCase());
+        return total + product.price
+    }, 0);
+
+    // Create the order
+    const order = new Order({
+        buyer: req.user.name,
+        seller: seller.name,
+        products: items.map(item => ({
+            name: item.name,
+        })),
+        totalPrice
+    });
+
+    await order.save();
+
+    res.status(StatusCodes.CREATED).json(order);
+});
+
+export { getListOfSellers, getSellerCatalog, createOrder };
--- src/routes/buyerRoutes.js
+++ src/routes/buyerRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getListOfSellers, getSellerCatalog } from '../controllers/buyer.js';
+import { getListOfSellers, getSellerCatalog, createOrder } from '../controllers/buyer.js';
 import { allowedTo, protect } from '../middleware/authorization.js';
 
 const router = express.Router();
 
 router.get('/list-of-sellers', protect, allowedTo(['buyer']), getListOfSellers);
 router.get('/seller-catalog/:seller_id', protect, allowedTo(['buyer']), getSellerCatalog);
+router.post('/create-order/:seller_id', protect, allowedTo(['buyer']), createOrder);
 
 
 
