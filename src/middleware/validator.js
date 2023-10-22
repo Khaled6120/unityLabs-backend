e6ae7b354ae9f87fd57269b61266d57e65247672
Khaled6120 <khalednaes3@gmail.com>
2023-10-22 12:47:49 +0300

    Add Central validator middleware

---
+++ src/middleware/validator.js
@@ -0,0 +1,16 @@
+import { validationResult } from 'express-validator';
+import { StatusCodes } from 'http-status-codes';
+
+const validatorMiddleware = (
+    req,
+    res,
+    next
+) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors: errors.array() });
+    }
+    next();
+};
+
+export default validatorMiddleware;
