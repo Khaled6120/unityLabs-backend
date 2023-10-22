8e043fb2007ecd2a4fc63737ca6013722c273bf9
Khaled6120 <khalednaes3@gmail.com>
2023-10-22 03:51:09 +0300

    Fix TODO, getting loggedInUser id by Protect MW

--- src/controllers/seller.js
+++ src/controllers/seller.js
@@ -12,17 +12,11 @@ import Catalog from '../models/catalog.js';
 // @access  ONLY SELLERS
 const createCatalog = asyncHandler(async (req, res, next) => {
 
-    // TODO: seller id must be taken from the token (not from the request body)
-    const { sellerId, name, price } = req.body;
-
-    // Check if seller exists
-    const seller = await User.findById(sellerId);
-    if (!seller) {
-        return next(new AppError(StatusCodes.NOT_FOUND, 'Seller not found'));
-    }
+    const { name, price } = req.body;
+    const loggedInUser = req.user;
 
     // Find existing catalog for the seller
-    const existingCatalog = await Catalog.findOne({ seller: sellerId });
+    const existingCatalog = await Catalog.findOne({ seller: loggedInUser._id });
 
     // If catalog exists, add the new product to the existing products array
     if (existingCatalog) {
@@ -32,7 +26,7 @@ const createCatalog = asyncHandler(async (req, res, next) => {
     } else {
         // If catalog does not exist, create a new catalog with the new product
         const products = [{ name, price }];
-        const catalog = await Catalog.create({ seller: sellerId, products });
+        const catalog = await Catalog.create({ seller: loggedInUser._id, products });
         res.status(StatusCodes.CREATED).json({ success: true, catalog });
     }
 });
